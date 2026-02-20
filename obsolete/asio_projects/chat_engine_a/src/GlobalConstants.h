bd1fb22bc542cf6c59ba7b6cb1109caf2cc165d3
marleeeeeey <marleeeeeey@gmail.com>
2026-02-20 23:37:39 +0300

    Remove obsolete: DebugLog, AsioProjects

--- obsolete/asio_projects/chat_engine_a/src/GlobalConstants.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-constexpr int WINDOW_WIDTH = 640;
-constexpr int WINDOW_HEIGHT = 480;
--- obsolete/asio_projects/network_connection_lib/include/network_connection/Handlers.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <functional>
-#include <span>
-#include <system_error>
-
-using MessageHandler = std::function<void(std::span<const std::uint8_t> data)>;
-using ErrorHandler = std::function<void(const std::error_code& ec)>;
